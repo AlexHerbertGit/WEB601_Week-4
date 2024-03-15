945c4be6d0e37149d313bb302874a5801d18ba32
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-03-15 14:36:23 +1300

    Converted jquery to vanilla js, started adding CSS to css file

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,6 @@
 $(function () {
     var $h1 = $('h1')
-    var $zip = $("input[name='zip")
+    var $zip = $("input[name='zip']")
 
     $('form').on("submit", function(event) {
         event.preventDefault()
@@ -19,3 +19,31 @@ $(function () {
         })
     })
 })
+
+//Vanilla JavaScript version
+
+document.addEventListener('DOMContentLoaded', function () {
+    var h1 = document.querySelector('h1');
+    var zip = document.querySelector("input[name='zip']");
+
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var zipCode = zip.ariaValueMax.trim();
+        h1.textContent = "Loading...";
+
+        fetch('/' + zipCode)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error('Network response failed');
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                var temperature = data.temperature;
+                h1.textContent = "It is " + temperature +  " in " + zipCode + ".";
+            })
+            .catch(function(error) {
+                h1.textContent = "Error!";
+            })
+    })
+});
