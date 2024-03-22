0b3d8052751ce1643b1ba140d88b96eaaef75790
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-03-22 14:48:31 +1300

    ADded error pages and handling in index.js

--- src/_mvc/index.js
+++ src/_mvc/index.js
@@ -19,4 +19,23 @@ app.response.message = (msg) => {
      msgSession.push(msg)
 }
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({extended: true}));
+
+//Error page
+//response.status(500).render('error')
+
+//Error 500
+app.use((req, res, next) => {
+    res.status(500).render('error', {url: req.originalUrl})
+});
+
+//404 error
+app.use((req, res, next) => {
+    res.status(404).render('404', {url: req.originalUrl})
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+});
